34b72ac41c1e80e28c10891af69dfc656d1acbdb
Angga ps <80311292+anggaps@users.noreply.github.com>
2022-04-18 11:08:18 +0700

    first commit

---
+++ lib/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/category_item.dart';
+import 'package:meal_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: [
+          ...DUMMY_CATEGORIES
+              .map((e) => CategoryItem(e.title, e.color))
+              .toList()
+        ],
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
